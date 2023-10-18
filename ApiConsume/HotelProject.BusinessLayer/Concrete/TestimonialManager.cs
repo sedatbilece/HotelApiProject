bf36fd4551681193921903c9d3471c642efe7dd9
sedatbilece <seda1tbilece@gmail.com>
2023-10-18 15:58:22 +0300

    add Managers

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,47 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        private readonly ITestimonialDal _testimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void TDelete(Testimonial entity)
+        {
+            _testimonialDal.Delete(entity);
+        }
+
+        public List<Testimonial> TGetAll()
+        {
+            return _testimonialDal.GetAll();
+        }
+
+        public Testimonial TGetById(int id)
+        {
+            return _testimonialDal.GetById(id);
+        }
+
+        public void TInsert(Testimonial entity)
+        {
+            _testimonialDal.Insert(entity);
+        }
+
+        public void TUpdate(Testimonial entity)
+        {
+            _testimonialDal.Update(entity);
+        }
+    }
+}

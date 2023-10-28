60bc3d554ef7f7ce3a391eb19e309c0164cc891d
sedatbilece <seda1tbilece@gmail.com>
2023-10-28 18:46:58 +0300

    add(UI/staff) : List staff

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5224/api/Staff");
+            if (response.IsSuccessStatusCode)
+            {
+                var JsonData  = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<Staff>>(JsonData);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ Frontend/HotelProject.WebUI/Models/Staff.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelProject.WebUI.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Ttile { get; set; }
+    }
+}
